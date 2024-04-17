0cfa84830fba2d7754cd086bb2fbaffefbf27294
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-04-17 11:29:38 +0200

    feat: Connection impl (first try).

--- src/main/java/EmuCheckConnection.java
+++ src/main/java/EmuCheckConnection.java
@@ -1,4 +1,16 @@
+import net.sf.yad2xx.Device;
+import net.sf.yad2xx.FTDIException;
+import net.sf.yad2xx.FTDIInterface;
+
 public class EmuCheckConnection {
+
+    private Device device = null;
+    private boolean connected = true;
+
+    public EmuCheckConnection() throws FTDIException {
+        Device[] devices = FTDIInterface.getDevices();
+        System.out.println("Connected devices: " + devices.length);
+    }
 
     public void disconnect() {
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,43 @@
-import java.io.*;
 import net.sf.yad2xx.FTDIException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args){
-		try{
-			boolean b = true;
-			EmuCheckConnection ecc 
- 				= new EmuCheckConnection();
-			BufferedReader ein = new BufferedReader(
-				new InputStreamReader(System.in));
-			while(b){
-				String eingabe = ein.readLine();
-				if (eingabe.equals("exit")){
-					ecc.disconnect();
-					b=false;
-				}
-				else if (eingabe.equals("connect")){
-					ecc.connect();
-				}
-				else if (eingabe.equals("pmode")){
-					ecc.sendProgrammingMode();
-				}
-				else if (eingabe.equals("seriennummer")){
-					ecc.sendRequest(
- 						MESSWERT.Seriennummer);
-				}
-				else if (eingabe.equals("text")){
-					ecc.sendRequest(MESSWERT.Text);
-				}
-				else if(eingabe.equals("")){
-				}
-				else{
-					System.out.println(
- 						"Falscher Befehl!");
-				}
- 			}
- 		}
-		catch(FTDIException ftdiExc) {
-			System.out.println("FTDIException");
-			ftdiExc.printStackTrace();
-		}	
-		catch(IOException ioExc) {
-			System.out.println("IOException");
-			ioExc.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            boolean b = true;
+            EmuCheckConnection ecc
+                    = new EmuCheckConnection();
+            BufferedReader ein = new BufferedReader(
+                    new InputStreamReader(System.in));
+            while (b) {
+                String input = ein.readLine();
+                if (input.equals("exit")) {
+                    ecc.disconnect();
+                    b = false;
+                } else if (input.equals("connect")) {
+                    ecc.connect();
+                } else if (input.equals("pmode")) {
+                    ecc.sendProgrammingMode();
+                } else if (input.equals("seriennummer")) {
+                    ecc.sendRequest(
+                            MESSWERT.Seriennummer);
+                } else if (input.equals("text")) {
+                    ecc.sendRequest(MESSWERT.Text);
+                } else if (input.equals("")) {
+                } else {
+                    System.out.println(
+                            "Falscher Befehl!");
+                }
+            }
+        } catch (FTDIException ftdiExc) {
+            System.out.println("FTDIException");
+            ftdiExc.printStackTrace();
+        } catch (IOException ioExc) {
+            System.out.println("IOException");
+            ioExc.printStackTrace();
+        }
+    }
 }
